538a3ff22585a4b73c4b8415e7cac3f4920fd45e
Bruno Expósito <bruno.exposito@gmail.com>
2021-09-17 15:22:19 +0000

    Agrego a Dao la posibilidad de actualizar una entidad

--- Core/Dao.php
+++ Core/Dao.php
@@ -21,10 +21,29 @@ abstract class Dao {
 			}
 		}
 
-		//SQL without the last ", "
 		$sql = "INSERT INTO " . $entity::TABLE . '(' . substr($columns, 0, -2) .
 				') VALUES(' . substr($values, 0, -2) . ');';
 		return $this->execute($sql, $where);
+	}
+
+	public function update(Entity $entity) {
+		$sql = 'UPDATE ' . $entity::TABLE . ' SET ';
+
+		$where = array();
+        //Foreach in the map
+        foreach ($entity->getMap() as $key => $value) {
+            $val = $entity->{$value};
+
+            //if has value
+            if (!empty($val)) {
+				$sql .= '`' . $key . '` = ';
+				$sql .= ':' . $key . ', ';
+                $where[':' . $key] = $val;
+            }
+        }
+
+		$sql = substr($sql, 0, -2) . ' WHERE ' . $entity::PK . '=' . $entity->getId() . ';';
+        return $this->execute($sql, $where);
 	}
 
 	private function execute(string $query, array $params = array()) {
